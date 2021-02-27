d27954097032a4e48e54630d24ad349d51f678ef
Stas <stanislav.yas@gmail.com>
2021-02-27 14:12:19 +0600

    displayResultTable() done

--- src/display-results.js
+++ src/display-results.js
@@ -1,18 +1,76 @@
-const {colours: cc, objectFromJsonFile} = require('./util');
+const {colours: cc} = require('./util');
 
-const displayResultTable = (rt) => {
-  const {cams} = rt.slave;
-  const camsIDs = Object.keys(cams);
-  console.log(`Результат анализа видеофайлов на: ${rt.slave.id} ( подкл. в/к - ${camsIDs.length} )`);
+function printHoursHeader(camsCount, fromTimeInSec, deepInHours, intervalInMinutes, intervalsCount, ident) {
+  const hourLabelLength = Math.floor(60 / intervalInMinutes);
+  let header = ` видеокамеры (${camsCount} шт.):`.padEnd(ident);
+  for (let index = 0; index < deepInHours; index++) {
+    const curTime = new Date((fromTimeInSec + 1) * 1000 - ((deepInHours - index) * 60 * 60 * 1000));
+    const curHourStr = new String(curTime.getHours()).padStart(2, '0');
+    header += curHourStr.padEnd(hourLabelLength);
+  }
+  header = header.padStart(ident + intervalsCount);
+  console.log(`${cc.bright}${header}${cc.reset}`);
 }
 
-const displayResults = (resultTables) => {
-  for (const rt of resultTables) {
-    displayResultTable(rt);
+/**
+ * Графическое отображение информации о наличии видеофрагментов
+ * @param {Array} flags Массив отметок о наличии видеофрагментов. Расположены в обратном временном порядке (сначала более ранние)
+ * @returns {String}
+ */
+function getFragmentsInfo(flags) {
+  const checkedChar = '◘';
+  const notCheckedChar = '∙';
+  let fInfo = new String();
+  for (let index = 0; index < flags.length; index++) {
+    const flag = flags[flags.length - 1 - index];
+    fInfo += flag ? checkedChar : notCheckedChar;
+  };
+  return fInfo;
+}
+
+function printCamInfo(timeMap, camID, camName, ident) {
+  const gap = 1; // зазор между camTitle и fragmentsInfo
+  const camIDstring = new String(camID).padStart(3);
+  const camTitle = `№ ${camIDstring} - ${camName}`.substring(0, ident - gap).padEnd(ident - gap);
+  const { intervalsFlags: flags, checkedFragmentsCount: cnt } = timeMap[camID];
+  const fragmentsInfo = getFragmentsInfo(flags);
+  let fgColor = cc.reset;
+  if (cnt === 0) {
+    fgColor = cc.fg.red;
+  } else if (cnt <= 2) {
+    fgColor = cc.fg.yellow;
   }
-};
+  console.log(`${fgColor}${camTitle} ${fragmentsInfo} ( ${cnt} фр.)`);
+}
 
-const resultTables = objectFromJsonFile('./misc/result.json', 'utf8');
+function displayResultTable(resultTable, ident) {
+  const { slave, intervalsCount, options, fromTimeInSec, timeMap } = resultTable;
+  const { deepInHours, intervalInMinutes } = options;
+  const { cams } = slave;
+  const camsIDs = Object.keys(cams);
 
-displayResults(resultTables);
+  console.clear();
+  console.log(`\nРезультат анализа индексов видеоархива на ${cc.bright}${slave.id}${cc.reset} за ${deepInHours} часов`);
+  const sinceTime = new Date((fromTimeInSec + 1) * 1000 - (deepInHours * 60 * 60 * 1000));
+  console.log(`с ${sinceTime.toLocaleString()} (интервал - ${intervalInMinutes} минут)\n`);
+  printHoursHeader(camsIDs.length, fromTimeInSec, deepInHours, intervalInMinutes, intervalsCount, ident);
 
+  for (const camID of camsIDs) {
+    const camName = cams[camID];
+    printCamInfo(timeMap, camID, camName, ident);
+  }
+}
+
+function displayResults(resultTables) {
+  const ident = 40;
+  for (const resultTable of resultTables) {
+    displayResultTable(resultTable, ident);
+  }
+}
+
+(function test(){
+  const resultTables = require('./util').objectFromJsonFile('./misc/result.json', 'utf8');
+  displayResults(resultTables);
+})();
+
+module.exports = {displayResults, displayResultTable};
